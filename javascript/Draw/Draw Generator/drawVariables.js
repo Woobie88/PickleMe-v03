c084eff2f4c495c2bdfdcb574c694ca4a8a1fc47
Woobie88 <brett.collins028@gmail.com>
2026-09-12 14:48:14 +1000

    Updating draw variables

--- javascript/Draw/Draw Generator/drawVariables.js
+++ javascript/Draw/Draw Generator/drawVariables.js
@@ -1,8 +1,11 @@
 // --- Draw Generation Weighting: slider presets ---
 const penaltyWeightPresets = [
-  { drawWeightValue: 0, label: 'Maximise partner & opponent rotation', partnerDuprGapWeight: 10,  partnerFrequencyWeight: 100,  opponentDuprGapWeight: 10,  opponentFrequencyWeight: 100},
-  { drawWeightValue: 1, label: 'Balanced draw', partnerDuprGapWeight: 50,  partnerFrequencyWeight: 50,  opponentDuprGapWeight: 50,  opponentFrequencyWeight: 50  },
-  { drawWeightValue: 2, label: 'Minimise DUPR differences for the match',  partnerDuprGapWeight: 10,  partnerFrequencyWeight: 100,  opponentDuprGapWeight: 100,  opponentFrequencyWeight: 10  }
+  { drawWeightValue: 0, label: 'Maximise partner & opponent rotation', partnerDuprGapWeight: 10, partnerFrequencyWeight: 100,  
+    opponentDuprGapWeight: 10,  opponentFrequencyWeight: 100},
+  { drawWeightValue: 1, label: 'Balanced draw', partnerDuprGapWeight: 50,  partnerFrequencyWeight: 50,  
+    opponentDuprGapWeight: 50,  opponentFrequencyWeight: 50  },
+  { drawWeightValue: 2, label: 'Minimise DUPR differences for the match',  partnerDuprGapWeight: 100,  partnerFrequencyWeight: 20,  
+    opponentDuprGapWeight: 100,  opponentFrequencyWeight: 20  }
 ];
 
 let duprGapWeight;
