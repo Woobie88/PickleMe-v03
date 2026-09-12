92e37109dfbf30c5f3a67f593668bd9945c67d6d
Woobie88 <brett.collins028@gmail.com>
2026-09-12 14:02:32 +1000

    Testing draw variables for keep it fair

---
+++ javascript/Draw/Draw Generator/drawVariables.js
@@ -0,0 +1,9 @@
+// --- Draw Generation Weighting: slider presets ---
+const penaltyWeightPresets = [
+  { drawWeightValue: 0, label: 'Maximise partner & opponent rotation', partnerDuprGapWeight: 10,  partnerFrequencyWeight: 100,  opponentDuprGapWeight: 10,  opponentFrequencyWeight: 100},
+  { drawWeightValue: 1, label: 'Balanced draw', partnerDuprGapWeight: 50,  partnerFrequencyWeight: 50,  opponentDuprGapWeight: 50,  opponentFrequencyWeight: 50  },
+  { drawWeightValue: 2, label: 'Minimise DUPR differences for the match',  partnerDuprGapWeight: 10,  partnerFrequencyWeight: 100,  opponentDuprGapWeight: 100,  opponentFrequencyWeight: 10  }
+];
+
+let duprGapWeight;
+let frequencyWeight;
