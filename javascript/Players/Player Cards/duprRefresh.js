76e1f81f8be550eb3670b043a5d963faeb26ac03
Woobie88 <brett.collins028@gmail.com>
2026-09-14 16:51:58 +1000

    Writing dupr refresh

--- javascript/Players/Player Cards/duprRefresh.js
+++ javascript/Players/Player Cards/duprRefresh.js
@@ -2,6 +2,8 @@ function playerDUPRrefresh() {
     // Get the event players
     const payload = window.cachedUserUniverse;
     console.log('The payload is',payload);
+    const duprDatabase = window.cachedUserUniverse.dupr || [];
+    console.log('The DUPR data is',duprDatabase);
 
     // Get DUPR data
 
