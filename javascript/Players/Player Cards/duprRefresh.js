a854b80115462752942d365f2bd30cd4911671aa
Woobie88 <brett.collins028@gmail.com>
2026-09-14 17:26:36 +1000

    Testing

--- javascript/Players/Player Cards/duprRefresh.js
+++ javascript/Players/Player Cards/duprRefresh.js
@@ -1,15 +1,29 @@
 function playerDUPRrefresh() {
     // Get the event players
     const players = window.cachedUserUniverse.players;
-    console.log('The players payload is',players);
-    const duprData = window.fetchDuprDatabaseFromFirestore();
-    console.log('DUPR database is',duprData);
+    console.log('The players payload is', players);
 
     // Get DUPR data
+    const duprData = await window.fetchDuprDatabaseFromFirestore();
+    console.log('DUPR database is', duprData);
+
+    // Get current DUPR rating for each player
     players.forEach(player => {
-        console.log('The player name is',player.Name,'DUPR ID',player.DUPRId);
-        let currentDUPRRate = duprData.find(e => String(e.DUPRId) === String(DUPRId));
-        console.log('Their current rating is',currentDUPRRate["DUPR Rating"]);
+        console.log(
+            'The player name is',
+            player.Name,
+            'DUPR ID',
+            player.DUPRId
+        );
+
+        const currentDUPRRate = duprData.find(
+            e => String(e.DUPRId) === String(player.DUPRId)
+        );
+
+        console.log(
+            'Their current rating is',
+            currentDUPRRate ? currentDUPRRate["DUPR Rating"] : 'No DUPR rating found'
+        );
     });
 
     // Update date Firestore
