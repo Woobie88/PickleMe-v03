df7a2906c58e162a45c3c0f0db29f75384aa199e
Woobie88 <brett.collins028@gmail.com>
2026-09-14 17:04:44 +1000

    Testing

--- javascript/Players/Player Cards/duprRefresh.js
+++ javascript/Players/Player Cards/duprRefresh.js
@@ -2,12 +2,8 @@ function playerDUPRrefresh() {
     // Get the event players
     const payload = window.cachedUserUniverse;
     console.log('The payload is',payload);
-    if (!window.cachedUserUniverse.dupr || window.cachedUserUniverse.dupr.length === 0) {
-        window.fetchDuprDatabaseFromFirestore().then(db => {
-            window.cachedUserUniverse.dupr = db;
-        });
-    }
-    console.log('DUPR database is',window.cachedUserUniverse.dupr);
+    const duprData = window.fetchDuprDatabaseFromFirestore();
+    console.log('DUPR database is',duprData);
 
     // Get DUPR data
 
