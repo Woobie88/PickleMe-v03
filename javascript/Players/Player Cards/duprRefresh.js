75c20eb5992d752997f2f693de5b53933cc28d33
Woobie88 <brett.collins028@gmail.com>
2026-09-14 16:42:28 +1000

    Function step for DUPR refresh

---
+++ javascript/Players/Player Cards/duprRefresh.js
@@ -0,0 +1,10 @@
+function playerDUPRrefresh() {
+    // Get the event players
+    console.log('The payload is',payload);
+
+    // Get DUPR data
+
+    // Update date Firestore
+
+    // Re render players screen
+}
