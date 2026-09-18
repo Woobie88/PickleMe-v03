d8cbb50960e205de6bb4ce850d353f29483d7450
Woobie88 <brett.collins028@gmail.com>
2026-09-18 17:20:30 +1000

    Testing the generation of partnerships

--- javascript/Draw/Draw Generator/drawGenerator.js
+++ javascript/Draw/Draw Generator/drawGenerator.js
@@ -341,6 +341,7 @@ function buildMatchRecord(m, idx, roundNumber, eventId, drawVersion, userEmail, 
 
 function generateGroupMatches(groupPlayers, courtNumbers, partnerCounts, opponentCounts, courtCounts, roundNumber, eventId, drawVersion, userEmail, scorers) {
   const partnerships = generateBestPartnerships(groupPlayers, partnerCounts, scorers);
+  console.log(`For round ${roundNumber}, the partnerships are ${partnerships}`);
   const matchups = generateBestMatchups(partnerships, opponentCounts, scorers);
   const courted = assignCourts(matchups, courtNumbers, courtCounts);
 
