db3a09f136b286336ccf54bf8e903033d76a6cb7
Woobie88 <brett.collins028@gmail.com>
2026-09-22 15:43:13 +1000

    Changing draw variables

--- javascript/Draw/Draw Generator/drawVariables.js
+++ javascript/Draw/Draw Generator/drawVariables.js
@@ -4,7 +4,7 @@ const penaltyWeightPresets = [
     opponentDuprGapWeight: 10,  opponentFrequencyWeight: 100, opponentDuprDelta: 0.5, opponentMinExpScore: 5  },
   { drawWeightValue: 1, label: 'Balanced draw', partnerDuprGapWeight: 50,  partnerFrequencyWeight: 50, partnerDuprDelta: 0.4,
     opponentDuprGapWeight: 50,  opponentFrequencyWeight: 50, opponentDuprDelta: 0.3, opponentMinExpScore: 7  },
-  { drawWeightValue: 2, label: 'Minimise DUPR differences for the match',  partnerDuprGapWeight: 50,  partnerFrequencyWeight: 50, partnerDuprDelta: 0.6,
+  { drawWeightValue: 2, label: 'Minimise DUPR differences for the match',  partnerDuprGapWeight: 75,  partnerFrequencyWeight: 25, partnerDuprDelta: 0.6,
     opponentDuprGapWeight: 100,  opponentFrequencyWeight: 10, opponentDuprDelta: 0.2, opponentMinExpScore: 8  }
 ];
 
