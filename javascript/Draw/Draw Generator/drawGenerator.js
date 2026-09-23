0736cd72cbb528119fbf3b6a05d9f12d29b21a8e
Woobie88 <brett.collins028@gmail.com>
2026-09-23 12:54:25 +1000

    Testing

--- javascript/Draw/Draw Generator/drawGenerator.js
+++ javascript/Draw/Draw Generator/drawGenerator.js
@@ -175,7 +175,7 @@ function makeScorers(drawBuildVariables) {
     partnerDuprDelta: drawBuildVariables.partnerDuprDelta ?? Infinity,
     opponentDuprDelta: drawBuildVariables.opponentDuprDelta ?? Infinity,
     maxGroupCandidates: drawBuildVariables.maxGroupCandidates ?? 10,
-    groupDuprGapWeight: drawBuildVariables.groupDuprGapWeight ?? 20,
+    groupDuprGapWeight: drawBuildVariables.groupDuprGapWeight ?? 40,
     partnerFrequencyWeight: drawBuildVariables.partnerFrequencyWeight,
     opponentFrequencyWeight: drawBuildVariables.opponentFrequencyWeight
   };
