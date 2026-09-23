ae85c4c251a5a4b3002f93fd8beba37582b7178f
Woobie88 <brett.collins028@gmail.com>
2026-09-23 12:21:59 +1000

    Testing

--- javascript/Draw/Draw Generator/drawGeneratorBalancedMatchUps.js
+++ javascript/Draw/Draw Generator/drawGeneratorBalancedMatchUps.js
@@ -13,7 +13,7 @@ function scoreGroupFormation(group, partnerCounts, opponentCounts, scorers) {
       const partnerRepeats = (partnerCounts[p1.PlayerID]?.[p2.PlayerID]) || 0;
       const opponentRepeats = (opponentCounts[p1.PlayerID]?.[p2.PlayerID]) || 0;
 
-      duprCost += duprGap * 10;                                // scorers.partnerDuprGapWeight;
+      duprCost += duprGap;
       freqCost += partnerRepeats * scorers.partnerFrequencyWeight
                 + opponentRepeats * scorers.opponentFrequencyWeight;
     }
