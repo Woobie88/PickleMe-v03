ce642237fd24aca1d7f14dbec9dd215d3085e324
Woobie88 <brett.collins028@gmail.com>
2026-09-23 12:15:54 +1000

    Fix for engine

--- javascript/Draw/Draw Generator/drawGeneratorBalancedMatchUps.js
+++ javascript/Draw/Draw Generator/drawGeneratorBalancedMatchUps.js
@@ -13,7 +13,7 @@ function scoreGroupFormation(group, partnerCounts, opponentCounts, scorers) {
       const partnerRepeats = (partnerCounts[p1.PlayerID]?.[p2.PlayerID]) || 0;
       const opponentRepeats = (opponentCounts[p1.PlayerID]?.[p2.PlayerID]) || 0;
 
-      duprCost += duprGap * partnerDuprGapWeight;
+      duprCost += duprGap * scorers.partnerDuprGapWeight;
       freqCost += partnerRepeats * scorers.partnerFrequencyWeight
                 + opponentRepeats * scorers.opponentFrequencyWeight;
     }
